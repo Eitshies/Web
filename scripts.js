8c9ae9d04ad34c9afc214ecae642c43bca44cb03
Herrin <ylianaerraoui@gmail.com>
2024-12-08 22:59:25 +0100

    Updating the website

--- scripts.js
+++ scripts.js
@@ -1,3 +1,13 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     console.log('The page is fully loaded');
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
